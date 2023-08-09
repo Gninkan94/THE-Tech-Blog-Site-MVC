87993cf05b0c2242f79d1945345e8abee0176a0b
Ibrahima Diallo <ibrahimadiallo@Ibrahimas-MBP.ht.home>
2023-08-09 17:58:45 -0400

    I added the serverjs file, i added the .env file for sensitive data to connect to my db and added the connectionjs file

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+require('dotenv').config(); // this will Import sensitive data from .env file
+const sequelize = process.env.JAWSDB_URL 
+  ? new Sequelize(process.env.JAWSDB_URL) // this is a JAWSDB_URL  for HEROKU
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { 
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
