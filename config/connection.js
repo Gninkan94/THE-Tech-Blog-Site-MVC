215ea3d72de5ff79f72c3ee6f464f90585f95531
Ibrahima Diallo <ibrahimadiallo@Ibrahimas-MBP.ht.home>
2023-08-12 12:01:39 -0400

    I added all files I need in everyfolder for the mvc model

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
-require('dotenv').config(); // this will Import sensitive data from .env file
+require('dotenv').config(); //this will Import sensitive data from .env file
 const sequelize = process.env.JAWSDB_URL 
-  ? new Sequelize(process.env.JAWSDB_URL) // this is a JAWSDB_URL  for HEROKU
+  ? new Sequelize(process.env.JAWSDB_URL) //this is a JAWSDB_URL  for HEROKU
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { 
       host: 'localhost',
       dialect: 'mysql',
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Importing required modules and dependencies
+// this will Importing required modules and dependencies
 const express = require("express");
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -6,11 +6,10 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers: require("./utils/helpers") });
-// Creating express app and setting port
-
+// this will Create express app and setting port
 const app = express();
 const PORT = process.env.PORT || 3001;
-// Setting up session object with secret, cookie, and store
+// this is Setting up session object with secret, cookie, and store
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
