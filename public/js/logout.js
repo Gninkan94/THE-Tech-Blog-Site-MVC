9ae4759a25fd716ba6c55be36106ccff8cf4e4aa
Ibrahima Diallo <ibrahimadiallo@Ibrahimas-MBP.ht.home>
2023-08-13 10:46:04 -0400

    I added code in files in the js and public folders

---
+++ public/js/logout.js
@@ -0,0 +1,19 @@
+// Logout function to send request to log out the user
+const techLogout = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/'); 
+    } else {
+      alert('Failed to log out.'); 
+    }
+  };
+  
+  // Event listener to the logout button
+  const chessLogoutButton = document.querySelector('#the-tech-logout');
+  if (chessLogoutButton) {
+    chessLogoutButton.addEventListener('click', techLogout);
+  }
